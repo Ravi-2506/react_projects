b014a453b13ff84c70a2def2f9cd12ab790f0f87
Ravi-25 <ravi@standav.com>
2019-01-11 18:53:30 +0530

    react projects

---
+++ react-Props/src/App.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import StateExp from "./StateExample.jsx";
+
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <StateExp/>
+        </div>
+    );
+  }
+}
+
+
+export default App;
