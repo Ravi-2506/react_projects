126695d8f4b6e5a5c37e73e7ffcf44d0c3ae3091
Ravi-25 <ravi@standav.com>
2019-01-25 17:00:01 +0530

    EOD

--- react-Props/src/PropChild.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class ChildOne extends React.Component {
-    someOtherFunction = () => {
-        this.props.setMuteActive(!this.props.muteActive);
-    }
-}
-
-export default ChildOne;
